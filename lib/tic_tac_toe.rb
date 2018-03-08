d29bcd9a6433d45a014a1678ae99e529003e9905
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-08 23:22:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  @index = input_to_index(@input)
+  @index = input_to_index(input)
   current_player(@board)
   @current_player = current_player(@board)
   if valid_move?(@board, @index)
